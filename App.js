14382b833e27c61a9a0216a16bb2d84b79fde064
francisco.peralta <francisco.peralta@vortex-it.com>
2021-06-19 12:00:07 -0300

    clase 2

--- App.js
+++ App.js
@@ -1,39 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
-import {flatListArray} from "./array"
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Pressable, SafeAreaView, TextInput } from "react-native";
+import { flatListArray } from "./array";
 
 export default function App(props) {
-  const [variable, setVariable] = useState("hola")
+  const [variable, setVariable] = useState("");
 
   const funcion = () => {
-    setVariable("hola 2")
-  }
+    setVariable("hola 2");
+  };
 
   useEffect(() => {
-    console.log(flatListArray)
-    return () => console.log("desmontado")
-  }, [])
+    console.log(flatListArray);
+    return () => console.log("desmontado");
+  }, []);
 
   useEffect(() => {
-    console.log("cambio variable ", variable)
-    return () => console.log("desmontado")
-  }, [variable])
+    console.log("cambio variable ", variable);
+    return () => console.log("desmontado");
+  }, [variable]);
 
   return (
-    <View style={styles.container}>
-      <Text>{variable}</Text>
-      <Pressable style={{padding: 10, backgroundColor:"red" }} onPress={() => funcion()}><Text>Cambiar</Text></Pressable>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontSize: 40, fontWeight: "bold", fontStyle: "italic", color: "red"}}>{variable}</Text>
+      <TextInput 
+        keyboardAppearance={"dark"}
+        keyboardType={"numeric"}
+        textContentType={"newPassword"}
+        autoCompleteType={"password"}
+        secureTextEntry={false}
+        multiline={true}
+        value={variable}
+        onChangeText={(text) => setVariable(text)}
+        placeholder={"Placeholder react native"}
+        placeholderTextColor={"blue"}
+        style={{backgroundColor: "#d3d3d3", width: "80%", height: 80}}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
 });
