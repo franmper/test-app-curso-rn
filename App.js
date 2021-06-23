69d9dc67451f828f0e7182a31de03d2cb4bf8ab3
francisco.peralta <francisco.peralta@vortex-it.com>
2021-06-23 20:51:48 -0300

    app

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Pressable, SafeAreaView, TextInput } from "react-native";
-import { flatListArray } from "./array";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  SafeAreaView,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  SectionList,
+  ScrollView,
+  Image,
+  Modal,
+  ActivityIndicator,
+  StatusBar
+} from "react-native";
+import { flatListArray, sectionListArray } from "./array";
+
+const altoStatusBar = StatusBar.currentHeight
 
 export default function App(props) {
-  const [variable, setVariable] = useState("");
+  const [variable, setVariable] = useState("hola");
+  const [visible, setVisible] = useState(false);
 
   const funcion = () => {
     setVariable("hola 2");
@@ -21,29 +38,17 @@ export default function App(props) {
   }, [variable]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={{fontSize: 40, fontWeight: "bold", fontStyle: "italic", color: "red"}}>{variable}</Text>
-      <TextInput 
-        keyboardAppearance={"dark"}
-        keyboardType={"numeric"}
-        textContentType={"newPassword"}
-        autoCompleteType={"password"}
-        secureTextEntry={false}
-        multiline={true}
-        value={variable}
-        onChangeText={(text) => setVariable(text)}
-        placeholder={"Placeholder react native"}
-        placeholderTextColor={"blue"}
-        style={{backgroundColor: "#d3d3d3", width: "80%", height: 80}}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={{fontSize: 40, fontWeight: "bold", fontStyle: "italic", color: "white"}}>{variable}</Text>
+      
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: altoStatusBar,
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "red"
   },
 });
